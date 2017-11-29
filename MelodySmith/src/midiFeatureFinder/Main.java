bc9a0c4ad540cda2809d42b289c79d5492666fd7
SunOB <sun.steven0@gmail.com>
2017-11-29 16:20:18 -0700

    Changed test file, started on writer

--- MelodySmith/src/midiFeatureFinder/Main.java
+++ MelodySmith/src/midiFeatureFinder/Main.java
@@ -8,8 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MidiReader r = new MidiReader();
-		r.readSequenceRaw(new File("midiReaderData/Test.mid"));
-		r.readSequence(new File("midiReaderData/Test.mid"));
+		r.readSequenceRaw(new File("midiReaderData/DC.mid"));
+		r.readSequence(new File("midiReaderData/DC.mid"));
 		ArrayList<ArrayList<Note>> test = r.getOrderedNotes();
 		System.out.println();
 	}
